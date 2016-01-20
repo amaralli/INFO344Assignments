0e3af0f690d49a497c59f9dbecf5f18c6f3513f5
amaralli <amaralli@uw.edu>
2016-01-20 13:05:50 -0800

    fix sql again

--- Assignment1/DoEverything.sql
+++ Assignment1/DoEverything.sql
@@ -4,7 +4,7 @@ drop table if exists movies;
 
 create table movies (title varchar(72) not null,released varchar(64),distributor varchar(64),genre varchar(32),rating varchar(10),gross varchar(15),tickets varchar(15),imdb_id varchar(15),id int auto_increment not null primary key);
 
-load data infile '~/info344assignments/assignment1/data/movies-2014.csv'
+load data infile '/var/www/html/info344assignments/assignment1/data/movies-2014.csv'
 into table movies
 fields terminated by ','
 optionally enclosed by '"'
